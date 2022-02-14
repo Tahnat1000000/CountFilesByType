dd40a55cd46df48b212fb27966abc627086dcc30
Yevdayev Ilan <92335928+Tahnat1000000@users.noreply.github.com>
2022-02-14 16:14:31 +0200

    Update CountFilesByType.py

--- CountFilesByType.py
+++ CountFilesByType.py
@@ -1,22 +1,26 @@
 import os, time
 
-dir_path = os.path.dirname(os.path.abspath(__file__))
-filestype = []
-typecounter = []
+def main():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    filestype = []
+    typecounter = []
 
-for root, dirs, files in os.walk(dir_path):
-    if root is dir_path:
-        for file in files:
-            filetype = file.split(".")[len(file.split("."))-1]
-            if not filestype.__contains__(filetype):
-                filestype.append(filetype)
-                typecounter.append(filetype)
-                typecounter.append(1)
-            else:
-                typecounter[typecounter.index(filetype)+1] += 1
+    for root, dirs, files in os.walk(dir_path):
+        if root is dir_path:
+            for file in files:
+                filetype = file.split(".")[len(file.split("."))-1]
+                if not filestype.__contains__(filetype):
+                    filestype.append(filetype)
+                    typecounter.append(filetype)
+                    typecounter.append(1)
+                else:
+                    typecounter[typecounter.index(filetype)+1] += 1
 
-for index in range(len(typecounter))[::2]:
-    print(typecounter[index] + ": " + str(typecounter[index+1]))
+    for index in range(len(typecounter))[::2]:
+        print(typecounter[index] + ": " + str(typecounter[index+1]))
 
 
-time.sleep(30)
+    time.sleep(30)
+    
+if __name__ == "__main__":
+    main()
